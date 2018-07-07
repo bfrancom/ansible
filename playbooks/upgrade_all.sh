28c4ac0360b73e21a2c379b42cd41252a10cb9fb
Ben Francom <bfrancom@gmail.com>
2018-07-07 14:55:51 -0600

    DEAL WITH IT

---
+++ playbooks/upgrade_all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ansible-playbook -v /home/bfrancom/development/ansible/playbooks/openbsd/openbsd_upgrade.yml
+ansible-playbook -v /home/bfrancom/development/ansible/playbooks/ubuntu/ubuntu.yml
